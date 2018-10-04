262b45400d112b5ab99bf29e4faff45de677dd91
Laurens Kuiper <laurens-kuiper@hotmail.com>
2018-10-04 12:28:27 +0200

    Werkcollege 4-10

--- src/test-independence.r
+++ src/test-independence.r
@@ -1,28 +1,34 @@
 # install.packages("devtools", repos='https://cran.r-project.org/')
 # install.packages('readr', repos='https://cran.r-project.org/')
 # install_github("jtextor/dagitty/r")
+# devtools::install_github("jtextor/dagitty/r")
 
 library(devtools)
 library(dagitty)
 library(readr)
 
-d <- read.csv("../data/bank-without-unknown.csv",sep=';')
+d <- read.csv("../data/bank-additional-full.csv",sep=';')
+d$pdays <- NULL
+d$previous <- NULL
+d$poutcome <- NULL
 
 fileName <- '../wack-model'
 dag <- read_file(fileName)
 g <- dagitty(dag)
 
-# impliedConditionalIndependencies(g)
+# ici <- impliedConditionalIndependencies(g)
+sink('../data/chisq.txt')
+localTests(g,d,type="cis.chisq")
+sink()
 
 # previous _||_ y | poutcome
-# Manually test the implication: HoursPerWeek _||_ Race | Immigrant
-chisq <- 0; df <- 0
-for( a in unique( d$Immigrant ) ){
-	tst <- chisq.test( d$previous[d$poutcome==a], d$y[d$poutcome==a] )
-	chisq <- chisq + tst$statistic
-	df <- df + tst$parameter
-}
-cat( chisq, df, "\n" )
-pchisq( chisq, df, lower.tail=FALSE )
+# chisq <- 0; df <- 0
+# for( a in unique( d$Immigrant ) ){
+# 	tst <- chisq.test( d$previous[d$poutcome==a], d$y[d$poutcome==a] )
+# 	chisq <- chisq + tst$statistic
+# 	df <- df + tst$parameter
+# }
+# cat( chisq, df, "\n" )
+# pchisq( chisq, df, lower.tail=FALSE )
 
 # localTests(g,d,type="cis.chisq")
