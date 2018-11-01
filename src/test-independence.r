0ed0b237de1ddd2b4e709917a09f7b89ab62856a
lnkuiper <laurens-kuiper@hotmail.com>
2018-11-01 12:17:11 +0100

    Added histogram plots

--- src/test-independence.r
+++ src/test-independence.r
@@ -1,43 +1,46 @@
-# install.packages("devtools", repos='https://cran.r-project.org/')
+# install.packages('devtools', repos='https://cran.r-project.org/')
 # install.packages('readr', repos='https://cran.r-project.org/')
-# install_github("jtextor/dagitty/r")
+# install_github('jtextor/dagitty/r')
 # install.packages('lavaan', repos='https://cran.r-project.org/')
-# devtools::install_github("jtextor/dagitty/r")
+# devtools::install_github('jtextor/dagitty/r')
 
 library(devtools)
 library(dagitty)
 library(lavaan)
 library(readr)
 
-d <- read.csv("../data/bank-additional-full.csv",sep=';')
-# cpi <- d$cons.price.idx
-# cpi_bin <- lapply(cpi, (function (x) 1))
-# evr <- d$emp.var.rate
-# evr_bin <-lapply(evr, (function (x) 1))
-# d$emp.var.rate <- evr_bin
-# # cpi_bin
-# d$cons_price_idx <- cpi_bin
+d <- read.csv('../data/bank-additional-full.csv',sep=';')
+str(d)
 
-# ding <- d$cons_price_idx
-# unique(lapply(ding, class))
+png('age.png')
+hist(d$age, breaks=20, xlab='Age', main='Age frequencies')
+dev.off()
 
-fileName <- './model.r'
-dag <- read_file(fileName)
-g <- dagitty(dag)
+png('emp_var_rate.png')
+hist(d$emp.var.rate, breaks=20, xlab='Employment variation rate (quarterly indicator)', main='Employment variation rate frequencies')
+dev.off()
 
-sink('../data/ici.txt')
-# impliedConditionalIndependencies(g)
-localTests(g,d,type="cis.chisq")
-sink()
+png('consumer_price_index.png')
+hist(d$cons.price.idx, breaks=20, xlab='Consumer price index (monthly indicator)', main='Consumer price index frequencies')
+dev.off()
 
-# previous _||_ y | poutcome
-# chisq <- 0; df <- 0
-# for( a in unique( d$Immigrant ) ){
-# 	tst <- chisq.test( d$previous[d$poutcome==a], d$y[d$poutcome==a] )
-# 	chisq <- chisq + tst$statistic
-# 	df <- df + tst$parameter
-# }
-# cat( chisq, df, "\n" )
-# pchisq( chisq, df, lower.tail=FALSE )
+png('consumer_confidence_index.png')
+hist(d$cons.conf.idx, breaks=20, xlab='Consumer confidence index (monthly indicator)', main='Consumer confidence index frequencies')
+dev.off()
 
-# localTests(g,d,type="cis.chisq")
+png('euribor3m.png')
+hist(d$euribor3m, breaks=20, xlab='Euribor rate (quarterly indicator)', main='Euribor rate frequencies')
+dev.off()
+
+png('nr_employed.png')
+hist(d$nr.employed, breaks=20, xlab='Number of employees (quarterly indicator)', main='Number of employees (x1000) frequencies')
+dev.off()
+
+# fileName <- './model.r'
+# dag <- read_file(fileName)
+# g <- dagitty(dag)
+
+# sink('../data/ici.txt')
+# # impliedConditionalIndependencies(g)
+# localTests(g,d,type='cis.chisq')
+# sink()
