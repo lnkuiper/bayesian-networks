e413fe625b5a02125bb266789f62fe13ce0978bc
lnkuiper <laurens-kuiper@hotmail.com>
2018-11-01 14:12:43 +0100

    Klaar.

--- src/test-independence.r
+++ src/test-independence.r
@@ -9,38 +9,38 @@ library(dagitty)
 library(lavaan)
 library(readr)
 
-d <- read.csv('../data/bank-additional-full.csv',sep=';')
+d <- read.csv('../data/bank-additional-full-binned.csv',sep=',')
 str(d)
 
-png('age.png')
-hist(d$age, breaks=20, xlab='Age', main='Age frequencies')
-dev.off()
+fileName <- './initial_model.r'
+dag <- read_file(fileName)
+g <- dagitty(dag)
 
-png('emp_var_rate.png')
-hist(d$emp.var.rate, breaks=20, xlab='Employment variation rate (quarterly indicator)', main='Employment variation rate frequencies')
-dev.off()
-
-png('consumer_price_index.png')
-hist(d$cons.price.idx, breaks=20, xlab='Consumer price index (monthly indicator)', main='Consumer price index frequencies')
-dev.off()
-
-png('consumer_confidence_index.png')
-hist(d$cons.conf.idx, breaks=20, xlab='Consumer confidence index (monthly indicator)', main='Consumer confidence index frequencies')
-dev.off()
-
-png('euribor3m.png')
-hist(d$euribor3m, breaks=20, xlab='Euribor rate (quarterly indicator)', main='Euribor rate frequencies')
-dev.off()
-
-png('nr_employed.png')
-hist(d$nr.employed, breaks=20, xlab='Number of employees (quarterly indicator)', main='Number of employees (x1000) frequencies')
-dev.off()
-
-# fileName <- './model.r'
-# dag <- read_file(fileName)
-# g <- dagitty(dag)
-
-# sink('../data/ici.txt')
-# # impliedConditionalIndependencies(g)
+sink('../data/ici.txt')
+impliedConditionalIndependencies(g)
 # localTests(g,d,type='cis.chisq')
-# sink()
+sink()
+
+# png('age.png')
+# hist(d$age, breaks=20, xlab='Age', main='Age frequencies')
+# dev.off()
+
+# png('emp_var_rate.png')
+# hist(d$emp.var.rate, breaks=20, xlab='Employment variation rate (quarterly indicator)', main='Employment variation rate frequencies')
+# dev.off()
+
+# png('consumer_price_index.png')
+# hist(d$cons.price.idx, breaks=20, xlab='Consumer price index (monthly indicator)', main='Consumer price index frequencies')
+# dev.off()
+
+# png('consumer_confidence_index.png')
+# hist(d$cons.conf.idx, breaks=20, xlab='Consumer confidence index (monthly indicator)', main='Consumer confidence index frequencies')
+# dev.off()
+
+# png('euribor3m.png')
+# hist(d$euribor3m, breaks=20, xlab='Euribor rate (quarterly indicator)', main='Euribor rate frequencies')
+# dev.off()
+
+# png('nr_employed.png')
+# hist(d$nr.employed, breaks=20, xlab='Number of employees (quarterly indicator)', main='Number of employees (x1000) frequencies')
+# dev.off()
