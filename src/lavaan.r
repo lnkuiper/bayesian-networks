2ae748c9368cec6c962672c9305485b1e8b7d3cf
lnkuiper <laurens-kuiper@hotmail.com>
2018-12-06 14:25:35 +0100

    Lavaan output

--- src/lavaan.r
+++ src/lavaan.r
@@ -9,9 +9,9 @@ dag <- read_file("./selfmade_mb.r")
 g.assumed <- dagitty(dag)
 m.assumed <- toString(g.assumed, "lavaan")
 
-# str(d)
 d <- as.data.frame(scale(d))
-# str(d)
 
 fit <- sem(m.assumed, d)
+sink('../data/selfmade_mb.txt')
 print(summary(fit))
+sink()
